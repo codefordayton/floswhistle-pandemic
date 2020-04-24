4faac49ace4872b925eb85f847a740eb59870516
David Best <dbest@miletwo.us>
2020-04-23 20:39:47 -0400

    Adding the splash page.

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-
-import ActionButton from 'shared/ActionButton';
 
 
 const HomePage = props => {
@@ -10,46 +7,29 @@ const HomePage = props => {
 		<div className="HomePage">
 			<div className="Hero">
 				<div className="HeroColumn">
-					<Link className="HeroButton" to="/map">CURIOUS?<br></br>CHECK STATS</Link>
-					<HashLink className="HeroButton" to="#ActionButton">SCROLL TO<br></br>FILE REPORT</HashLink>
+					<HashLink className="HeroButton" to="#social">PLEASE<br/>STAND BY<br/><br/>Flo is<br/>pivoting</HashLink>
 				</div>
 			</div>
 
 			<div className="content">
 				<p>
-				<b>Flo's Whistle: Pandemic</b> is a secure, anonymous platform
-				where direct patient care providers in the U.S. can report, once
-				a day, on workplace deficits that are endangering them and their
-				patients.
-				</p>
+					Flo’s Whistle is being reconfigured as <strong>Flo’s Whistle - Pandemic</strong>. As always, the Whistle will be a secure, anonymous reporting platform, but we’re expanding our reporting capacity and focusing on two key coronavirus issues - shortages and testing. In addition to nurses, we’re inviting all licensed/certified direct care providers, working in the pre-hospital, acute cate and long term care settings to report, once a day, on these issues.</p>
 				<p>
-				The COVID-19 pandemic is exposing many lethal faults in our
-				healthcare system. Direct care providers can see, from within
-				the system, where dangers lie. As witnesses on the front lines,
-				if we can report our first hand knowledge, we can contribute to
-				a fuller understanding of the pandemic situation as it unfolds.
-				</p>
-				<p>
-				This independent data collection experiment builds on the 2018
-				nurse staffing project, Flo’s Whistle. Reports are aggregated and
-				displayed on a dashboard, which includes a map of the U.S. We do
-				not ask for any personally identifiable data.
-				</p>
-				<p>
-				With the Flo’s Whistle projects, we’re following the example of
-				pioneering statistician, public health advocate and nurse,
-				Florence Nightingale. Her research and visual display of data
-				revealed patterns in causes of death among soldiers in the Crimean
-				War, lighting the way for rational policy reforms and dramatically
-				reducing suffering and loss of life.
-				</p>
-				<p>
-				We hope to do the same.
-				</p>
+					Direct care providers can see, from within the system, where dangers lie. As witnesses on the front lines, if we can report our first hand knowledge, we can contribute to a fuller understanding of the pandemic situation as it unfolds. Follow for launch announcement.</p>
 			</div>
 
-			<div id="ActionButton"></div>
-			<ActionButton/>
+			<div id="social" className="social">
+				<p>
+					Thank you for all you’re doing to care for your patients and each other during this pandemic.
+                        	</p>
+
+                        	<div className="socials">
+                                	<a href="https://www.facebook.com/flo.nightingale.3344"><i className="fa fa-facebook-f"></i></a>
+                                	<a href="https://twitter.com/F_NightingaleRN"><i className="fa fa-twitter"></i></a>
+                                	<a href="https://www.instagram.com/FloNightingaleRN/"><i className="fa fa-instagram"></i></a>
+                                	<a href="mailto:flo@codefordayton.org"><i className="fa fa-envelope"></i></a>
+                        	</div>
+			</div>
 		</div>
 	);
 };
